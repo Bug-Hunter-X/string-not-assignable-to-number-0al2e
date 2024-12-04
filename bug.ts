c8298b709e5bf2b404eb08919f9e9fd984d1d225
BugHunter-X <askzenai@gmail.com>
2024-12-04 03:52:09 +0100

    Add bug.ts file

---
+++ bug.ts
@@ -0,0 +1,6 @@
+function combine(arr1: number[], arr2: number[]): number[] {
+  return [...arr1, ...arr2];
+}
+
+const result = combine([1, 2, 3], [4, 5, 6]);
+console.log(result); // Output: [1, 2, 3, 4, 5, 6]
